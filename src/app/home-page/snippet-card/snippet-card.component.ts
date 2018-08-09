3764c10e15690cd7bcaf494cf4f98199ca5e7c0b
mandeep1012 <mandeep1154@gmail.com>
2018-08-10 00:39:28 +0530

    a5L3s9

--- src/app/home-page/snippet-card/snippet-card.component.ts
+++ src/app/home-page/snippet-card/snippet-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Snippet } from '../models/snippet';
 
 @Component({
   selector: 'app-snippet-card',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SnippetCardComponent implements OnInit {
 
+  @Input() snippet: Snippet;
+  showModal = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  showDetails = () => {
+    this.showModal = true;
+  }
 }
